fc1306317db9b73acf12dd37def8ec2da2d27a04
Jerome Ross <jross@MBPM2-C65XHKNW25-ROSS.local>
2025-04-25 09:40:18 -0400

    Revised Changes

--- app/src/main/java/com/vipulasri/jetinstagram/ui/components/VideoPlayer.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/components/VideoPlayer.kt
@@ -8,64 +8,41 @@ import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
-import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
-import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.C
 
 @Composable
 fun VideoPlayer(uri: Uri) {
     val context = LocalContext.current
 
     val exoPlayer = remember {
-        SimpleExoPlayer.Builder(context)
-            .build()
-            .apply {
-                val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
-                    context,
-                    Util.getUserAgent(context, context.packageName)
-                )
-
-                val source = ProgressiveMediaSource.Factory(dataSourceFactory)
-                    .createMediaSource(uri)
-
-                this.prepare(source)
-            }
+        SimpleExoPlayer.Builder(context).build().apply {
+            val mediaItem = MediaItem.fromUri(uri)
+            setMediaItem(mediaItem)
+            prepare()
+            playWhenReady = true
+            videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
+            repeatMode = Player.REPEAT_MODE_ONE
+        }
     }
 
-    exoPlayer.playWhenReady = true
-    exoPlayer.videoScalingMode = C.VIDEO_SCALING_MODE_SCALE_TO_FIT_WITH_CROPPING
-    exoPlayer.repeatMode = Player.REPEAT_MODE_ONE
-
-/*    AndroidView(factory = {
-        PlayerView(context).apply {
-            hideController()
-            useController = false
-            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-
-            player = exoPlayer
-            layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        }
-    })*/
-
-    DisposableEffect(AndroidView(factory = {
-        PlayerView(context).apply {
-            hideController()
-            useController = false
-            resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
-
-            player = exoPlayer
-            layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-        }
-    })) {
+    DisposableEffect(
+        AndroidView(factory = {
+            PlayerView(context).apply {
+                hideController()
+                useController = false
+                resizeMode = AspectRatioFrameLayout.RESIZE_MODE_ZOOM
+                player = exoPlayer
+                layoutParams = FrameLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+            }
+        })
+    ) {
         onDispose {
             exoPlayer.release()
         }
     }
-
 }
